0e5186165367a206f478a89abb30e6182b829095
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-10-12 00:38:03 +0600

    added responsive card

--- src/Pages/HotJobsCard.jsx
+++ src/Pages/HotJobsCard.jsx
@@ -1,7 +1,8 @@
 import { FaMapLocationDot } from "react-icons/fa6";
+import { HiCurrencyBangladeshi } from "react-icons/hi";
 
 const HotJobsCard = ({ job }) => {
-    const { responsibilities, company, requirements, description, salaryRange, title, category, company_logo, location, jobType, } = job;
+    const { responsibilities, currency, company, requirements, description, salaryRange, title, category, company_logo, location, jobType, } = job;
     return (
         <div className="card bg-base-100 shadow-sm">
             <div className="flex gap-3 m-2">
@@ -9,7 +10,7 @@ const HotJobsCard = ({ job }) => {
                     <img
                         className="w-18"
                         src={company_logo}
-                        alt="Shoes" />
+                        alt="Logo" />
                 </figure>
                 <div>
                     <h2 className="text-2xl">{company}</h2>
@@ -17,10 +18,21 @@ const HotJobsCard = ({ job }) => {
                 </div>
             </div>
             <div className="card-body">
-                <h2 className="card-title">Card Title</h2>
-                <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+                <h2 className="card-title">{title}
+                    <div className="badge badge-secondary">NEW</div>
+                </h2>
+                <p>{description}</p>
+                <div className="flex flex-wrap gap-3">
+                    {
+                        requirements.map(requirement =>
+                            <p className="text-center border p-1 rounded-md  hover:text-pink-600"
+                            >{requirement}
+                            </p>)
+                    }
+                </div>
+                <div className="card-actions justify-end items-center">
+                    <p className="flex items-center gap-1">Salary : <HiCurrencyBangladeshi /> {salaryRange.min}-{salaryRange.max} {salaryRange.currency}</p>
+                    <button className="btn btn-primary mt-2">Apply Now</button>
                 </div>
             </div>
         </div>
