eb792a90106174fce48dccca9d59071e688e0939
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-10-16 20:00:20 +0600

    added new form

--- src/Pages/AddJob/AddJob.jsx
+++ src/Pages/AddJob/AddJob.jsx
@@ -13,6 +13,38 @@ const AddJob = () => {
 
                     <label className="label font-bold text-cyan-500">Job Location</label>
                     <input type="text" name="location" className="input w-2xl" placeholder="Job Location" />
+                    {/* jobs field */}
+                    <div className="form-control mt-2">
+                        <label className="label">
+                            <span className="label-text font-bold text-cyan-500">Job Field</span>
+
+                            <select defaultValue="Pick a font" className="select select-ghost">
+                                <option disabled={true}>Pick a Field</option>
+                                <option>Engineering</option>
+                                <option>Finance</option>
+                                <option>Accounts</option>
+                                <option>Management</option>
+                            </select>
+                        </label>
+                    </div>
+
+                    {/* jobs type */}
+                    <div className="form-control mt-2">
+                        <label className="label">
+                            <span className="label-text font-bold text-cyan-500">Job Type</span>
+
+                            <select defaultValue="Pick a font" className="select select-ghost">
+                                <option disabled={true}>Pick a Job Type</option>
+                                <option>Remote</option>
+                                <option>Full-Time</option>
+                                <option>Part-Time</option>
+                                <option>Internship</option>
+                            </select>
+                        </label>
+                    </div>
+
+                    {/* Salary Range */}
+
 
                     <button className="btn btn-primary mt-4">PostNow</button>
                 </fieldset>
